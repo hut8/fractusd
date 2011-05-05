c103f7c0ceeda5ce23603a76d4104c3c52405ed9
Liam Bowen <bowenl2@cs.rpi.edu>
2011-05-05 05:27:28 -0400

    I can't believe I thought the text file thing was a good idea..  I have no idea how to test the static stuff right, also.

---
+++ tests/fractus/tests/DatabaseTest.java
@@ -0,0 +1,54 @@
+package fractus.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class DatabaseTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAddContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testVerifyContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testListNonreciprocalContacts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAuthenticate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInvalidateLocation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLocateUser() {
+		fail("Not yet implemented");
+	}
+
+}
